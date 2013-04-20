94af3e81d471673165d9c4dabeeff40eb7a9b230
Marc Phillips <Marc.Phillips@Prodigy.net>
2013-04-20 12:53:30 -0400

    Fixup

--- config.js
+++ config.js
@@ -2,7 +2,6 @@
 // data source for all the repositories we want to pull from.
 var config = {
     repositories: [
-<<<<<<< HEAD
 //        {
 //            type: 'NASA',
 //            source: 'http://opensource.gsfc.nasa.gov/projects/DQSS/index.php',
@@ -11,21 +10,12 @@ var config = {
         {
             type: 'GIT',
             source: 'http://github.com/nasa/mct.git',
-=======
-        {
-            source: 'http://opensource.gsfc.nasa.gov/projects/DQSS/index.php',
-            target: 'https://github.com/NASATest/DQSS.git'
-        },
-        {
-            source: 'http://github.com/nasa/mct',
->>>>>>> Adding a check for .git repos only.  Going on this assumption for now.
             target: 'https://github.com/NASATest/mct.git'
         },
         {
             type: 'GIT',
             source: 'http://nasa.github.com/CertWare.git',
             target: 'https://github.com/NASATest/CertWare.git'
-<<<<<<< HEAD
 //        },
 //        {
 //            type: 'APACHE',
@@ -61,36 +51,6 @@ var config = {
 //            type: 'GIT',
 //            source: 'https://github.com/sunpy/sunpy.git',
 //            target: 'https://github.com/NASATest/sunpy.git'
-=======
-        },
-        {
-            source: 'http://svn.apache.org/repos/asf/oodt',
-            target: 'https://github.com/NASATest/oodt.git'
-        },
-        {
-            source: 'http://sourceforge.net/projects/datacasting/',
-            target: 'https://github.com/NASATest/datacasting.git'
-        },
-        {
-            source: 'http://sourceforge.net/projects/ion-dtn/',
-            target: 'https://github.com/NASATest/ion-dtn.git'
-        },
-        {
-            source: 'http://sourceforge.net/projects/dptoolkit/',
-            target: 'https://github.com/NASATest/dptoolkit.git'
-        },
-        {
-            source: 'http://sourceforge.net/projects/coreflightexec/files/cFE-6.1.1/',
-            target: 'https://github.com/NASATest/cFE-6.1.1.git'
-        },
-        {
-            source: 'http://github.com/OpenVSP/OpenVSP',
-            target: 'https://github.com/NASATest/OpenVSP.git'
-        },
-        {
-            source: 'https://github.com/sunpy/sunpy',
-            target: 'https://github.com/NASATest/sunpy.git'
->>>>>>> Adding a check for .git repos only.  Going on this assumption for now.
         }
     ]
 };
