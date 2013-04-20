3cc9ebeeb3191d99b1ec6e27cd634977c9ac87af
Marc Phillips <Marc.Phillips@Prodigy.net>
2013-04-20 11:58:11 -0400

    Added Source Type
    Repository.Type should indicate where the source is being copied from
    (i.e. GIT is from GITHub).

--- config.js
+++ config.js
@@ -3,42 +3,52 @@
 var config = {
     repositories: [
 //        {
+//            type: 'GIT',
 //            source: 'http://opensource.gsfc.nasa.gov/projects/DQSS/index.php',
 //            target: 'https://github.com/NASATest/DQSS.git'
 //        },
         {
+            type: 'GIT',
             source: 'http://github.com/nasa/mct',
             target: 'https://github.com/NASATest/mct.git'
         },
         {
+            type: 'GIT',
             source: 'http://nasa.github.com/CertWare',
             target: 'https://github.com/NASATest/CertWare.git'
 //        },
 //        {
+//            type: 'GIT',
 //            source: 'http://svn.apache.org/repos/asf/oodt',
 //            target: 'https://github.com/NASATest/oodt.git'
 //        },
 //        {
+//            type: 'GIT',
 //            source: 'http://sourceforge.net/projects/datacasting/',
 //            target: 'https://github.com/NASATest/datacasting.git'
 //        },
 //        {
+//            type: 'GIT',
 //            source: 'http://sourceforge.net/projects/ion-dtn/',
 //            target: 'https://github.com/NASATest/ion-dtn.git'
 //        },
 //        {
+//            type: 'GIT',
 //            source: 'http://sourceforge.net/projects/dptoolkit/',
 //            target: 'https://github.com/NASATest/dptoolkit.git'
 //        },
 //        {
+//            type: 'GIT',
 //            source: 'http://sourceforge.net/projects/coreflightexec/files/cFE-6.1.1/',
 //            target: 'https://github.com/NASATest/cFE-6.1.1.git'
 //        },
 //        {
+//            type: 'GIT',
 //            source: 'http://github.com/OpenVSP/OpenVSP',
 //            target: 'https://github.com/NASATest/OpenVSP.git'
 //        },
 //        {
+//            type: 'GIT',
 //            source: 'https://github.com/sunpy/sunpy',
 //            target: 'https://github.com/NASATest/sunpy.git'
         }
