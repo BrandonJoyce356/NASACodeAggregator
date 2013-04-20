526fa5662b2e9812b55c0dad6494db3efeccaf63
Brandon Joyce <Brandon@sonerdy.com>
2013-04-20 11:12:38 -0400

    Just adding some test config and a main loop.

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 var config = require('./config')
-var test = "test";
-console.log(test);
+
+//For now we just loop through the repositories and sync them to a target!
+for (var i=0;i < config.repositories.length;i++)
+{
+    var repository = config.repositories[i];
+    console.log('This is where we check ' + repository.source + ' for changes');
+    console.log('Then we sync to ' + repository.target);
+}
--- config.js
+++ config.js
@@ -1,1 +1,12 @@
-var config = {};
+//This config file is temporary.  We are probably going to have a more robust
+// data source for all the repositories we want to pull from.
+var config = {
+    repositories: [
+        {
+            source: 'https://github.com/nasa/mct.git',
+            target: 'https://github.com/BrandonJoyce356/NASATestSourceRepository.git'
+        }
+    ]
+};
+
+module.exports = config;
