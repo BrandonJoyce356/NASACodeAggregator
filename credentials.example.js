bae3b01d8a899460f8330dba3cf3b2e50ebccea5
Brandon Joyce <Brandon@sonerdy.com>
2013-04-21 09:10:59 -0400

    Removing the credentials thing, probably not needed now.

--- credentials.example.js
+++
@@ -1,9 +0,0 @@
-//these are credentials for the push to git, copy to credentials.js
-var credentials =
-    {
-        username: 'yourusername',
-        password: 'yourpassword'
-    }
-;
-
-module.exports = credentials;
